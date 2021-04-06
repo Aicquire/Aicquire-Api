e3bca593273854771e568ad704a9c5c3f5a40e9a
Jesury Thomas <jesurythomas@gmail.com>
2021-04-06 23:44:47 +0800

    Apply quick fix

--- src/models/employee.jobpositions.entity.ts
+++ src/models/employee.jobpositions.entity.ts
@@ -3,8 +3,10 @@ import { Questionnaire } from '../models/employee.jobpositions.interface';
 @Entity()
 export class JobPositions {
   @ObjectIdColumn() id: ObjectID;
-  @Column() position: string;
+  @Column() position: String;
   @Column() entries: Array<Questionnaire>;
+  @Column() competencies: Array<String>;
+  @Column() vacant: number
 
   constructor(jobPositions?: Partial<JobPositions>) {
     Object.assign(this, jobPositions);
